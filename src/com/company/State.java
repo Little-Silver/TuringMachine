fa938bacf2a79cf6e77521fb4e20689b010d10d4
Little-Silver <pascal.isliker@hotmail.com>
2021-04-27 17:18:16 +0200

    Updated TM addState

--- src/com/company/State.java
+++ src/com/company/State.java
@@ -7,10 +7,20 @@ public class State {
 
     private int stateId;
     private Map<TransactionFunction, Integer> functionMap = new HashMap<>();
+    private boolean isAccepting = false;
 
     public State(int stateId, Map<TransactionFunction, Integer> functionMap) {
         this.stateId = stateId;
         this.functionMap = functionMap;
     }
 
+    public State(int stateId, Map<TransactionFunction, Integer> functionMap, boolean isAccepting) {
+        this.stateId = stateId;
+        this.functionMap = functionMap;
+        this.isAccepting = isAccepting;
+    }
+
+    public boolean isAccepting() {
+        return isAccepting;
+    }
 }
--- src/com/company/TuringMachine.java
+++ src/com/company/TuringMachine.java
@@ -10,12 +10,28 @@ import static com.company.Direction.RIGHT;
 
 public class TuringMachine {
 
-    private static final int state = 0;
+    private State currentState;
     List<State> stateList = new ArrayList<>();
 
     public TuringMachine() {
         state00();
         state01();
+    }
+
+    public void addState(int stateId, Map<TransactionFunction, Integer> functionMap){
+        State state = new State(stateId, functionMap);
+        if(stateId == 0){
+            currentState = state;
+        }
+        stateList.add(state);
+    }
+
+    public void runTM(String input, int startPos){
+        Tape tape = new Tape(input, startPos);
+
+        while(!currentState.isAccepting()){
+
+        }
     }
 
     private void state00() {
@@ -26,14 +42,6 @@ public class TuringMachine {
     }
 
     private void state01() {
-        Map<TransactionFunction, Integer> functionMap = new HashMap<>();
-        functionMap.put(new TransactionFunction('0', '0', RIGHT), 1);
-        functionMap.put(new TransactionFunction('1', '1', RIGHT), 1);
-        functionMap.put(new TransactionFunction(' ', ' ', LEFT), 2);
-        stateList.add(new State(1, functionMap));
-    }
-
-    private void state02() {
         Map<TransactionFunction, Integer> functionMap = new HashMap<>();
         functionMap.put(new TransactionFunction('0', '0', RIGHT), 1);
         functionMap.put(new TransactionFunction('1', '1', RIGHT), 1);
